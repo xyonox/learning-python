7e4218ccd39e669e5018198af9bc73ef48e2bb77
einKampfLama <93388182+einKampfLama@users.noreply.github.com>
2024-07-27 13:14:42 +0200

    editable labels

--- basics/tinker/Gui.py
+++ basics/tinker/Gui.py
@@ -1,8 +1,39 @@
 from tkinter import *
 from tkinter import ttk
 root = Tk()
-frame = ttk.Frame(root, padding=20)
-frame.grid()
-ttk.Label(frame, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+root.title("My First Gui")
+root.geometry("500x500")
+
+def submit():
+    lab.config(text=en.get())
+    en.delete(0, 'end')
+def submit2():
+    lab2.config(text=tt.get("1.0", 'end-1c'))
+    tt.delete("1.0", 'end')
+
+
+
+lab = Label(root, text='hey')
+lab.pack()
+lab.config(text='ni na nu')
+
+
+
+en = Entry(root)
+en.pack()
+
+
+b = Button(text="submit", command=submit)
+b.pack()
+
+lab2 = Label(text="")
+lab2.pack()
+tt = Text(root, width=40, height=10)
+tt.pack()
+
+b2 = Button(text="submit", command=submit2)
+b2.pack()
+
+
 root.mainloop()
