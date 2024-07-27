6f72ac5de8d4a0786274d7314d4121de116684f4
einKampfLama <93388182+einKampfLama@users.noreply.github.com>
2024-07-27 14:28:37 +0200

    Create file

--- projects/Editor/Main.py
+++ projects/Editor/Main.py
@@ -1,11 +1,10 @@
-
 from tkinter import *
 from tkinter import ttk, filedialog
 import tkinter as tk
 
 root = Tk()
 
-file_path = ""
+file_path = None
 
 def on_closing():
     save()
@@ -13,27 +12,45 @@ def on_closing():
     root.destroy()
 
 def save():
-    if file_path == "":
-        fp = filedialog.askopenfilename()
+    global file_path
+    print(file_path)
+    if file_path is None:
+        print("hey")
+        fp = filedialog.asksaveasfilename(defaultextension=".txt",
+                                          filetypes=[("Textdateien", "*.txt"), ("Alle Dateien", "*.*")])
 
-        print(textfeld.get("1.0", 'end-1c'))
-        with open(fp, "w") as f:
-
-            f.write(textfeld.get("1.0", 'end-1c'))
+        if fp:
+            file_path = fp
+            print(textfeld.get("1.0", 'end-1c'))
+            with open(file_path, "w") as f:
+                f.write(textfeld.get("1.0", 'end-1c'))
     else:
         with open(file_path, "w") as f:
             f.write(textfeld.get("1.0", 'end-1c'))
 
+def create_file():
+    global file_path
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt",
+                                            filetypes=[("Textdateien", "*.txt"), ("Alle Dateien", "*.*")])
+    if file_path:
+        textfeld.delete("1.0", 'end')
+        root.title(file_path)
+
 def on_key_event(event):
+    global file_path
     if (event.state & 0x8) and event.keysym == 'l':
         file_path = filedialog.askopenfilename()
         print("Selected file:", file_path)
 
-        with open(file_path, "r") as f:
-            textfeld.delete("1.0", 'end')
-            textfeld.insert("1.0" ,f.read())
+        if file_path:
+            with open(file_path, "r") as f:
+                textfeld.delete("1.0", 'end')
+                textfeld.insert("1.0", f.read())
+                root.title(file_path)
     elif (event.state & 0x8) and event.keysym == 's':
         save()
+    elif (event.state & 0x8) and event.keysym == 'c':
+        create_file()
 
 if __name__ == "__main__":
     root.protocol("WM_DELETE_WINDOW", on_closing)
