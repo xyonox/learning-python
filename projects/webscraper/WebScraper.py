4185119f4cabf9963c7de68ed2868128bdc2c110
einKampfLama <93388182+einKampfLama@users.noreply.github.com>
2024-07-28 13:41:59 +0200

    BeautifulSoup
    filter the output

--- projects/webscraper/WebScraper.py
+++ projects/webscraper/WebScraper.py
@@ -1,4 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 rq = requests.get("https://oxylabs.io/")
-print(rq.text)
+soup = BeautifulSoup(rq.text, 'html.parser')
+
+
+blog_titles = soup.find_all('section', class_='enwhltf0')
+for title in blog_titles:
+    print(title.text)
