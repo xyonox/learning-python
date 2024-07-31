80a0024000f3b0efd8b76a2f8de42dda9e4f5f07
einKampfLama <93388182+einKampfLama@users.noreply.github.com>
2024-07-31 13:04:04 +0200

    Save stuff and added new element to save

--- projects/webscraper/WebScraper.py
+++ projects/webscraper/WebScraper.py
@@ -16,13 +16,20 @@ for title in blog_titles:
 driver = webdriver.Firefox()
 driver.get("https://sandbox.oxylabs.io/products")
 results = []
+prices = []
 content = driver.page_source
 soup = BeautifulSoup(content, "html.parser")
 
 for e in soup.findAll(attrs={"class": "product-card"}):
     name = e.find("h4")
+    price = e.find(attrs={"class": "price-wrapper"})
     results.append(name.text)
+    prices.append(price.text)
 
 print(results)
+
+# saving ^^
+df = pd.DataFrame({'Names': results, 'Prices': prices})
+df.to_csv('names.csv', index=False, encoding='utf-8')
 
 
