1cc04585c1c4e592833e0c7dca62c0dc3485e75f
einKampfLama <93388182+einKampfLama@users.noreply.github.com>
2024-08-01 11:31:21 +0200

    Blog -> create func

--- projects/blog/__main__.py
+++ projects/blog/__main__.py
@@ -1,4 +1,5 @@
 from src import setup
+from src.blog import Blog
 from src.user import User
 
 if __name__ == "__main__":
@@ -7,3 +8,7 @@ if __name__ == "__main__":
     setup.init()
     User.create("xyonox2", "ichbinschlaui2")
     print(User.exists("xyonox"))
+
+    Blog.create("xyonox", "Python lernen", "ich lerne hier py\n"
+                                           "du?\n"
+                                           "nein? weg mit dir D:")
---
+++ projects/blog/src/blog/Blog.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+from src.user import User
+
+
+def exists(title):
+    query = "SELECT COUNT(*) FROM blog WHERE title = ?"
+
+    db = sqlite3.connect("/Users/carl/Documents/GitHub/learning-python/projects/blog/blog.db")
+    cursor = db.cursor()
+
+    cursor.execute(query, (title,))
+    count = cursor.fetchone()[0]
+
+    if count > 0:
+        return True
+    else:
+        return False
+
+def create(user, title, content):
+    if exists(title):
+        print("-- noooo the title dont cheat her content")
+        return False
+    if not User.exists(user):
+        print("-- bro have no account? bruh")
+    else:
+        query = "INSERT INTO blog VALUES (?, ?, ? )"
+
+        db = sqlite3.connect("/Users/carl/Documents/GitHub/learning-python/projects/blog/blog.db")
+        cursor = db.cursor()
+
+        cursor.execute(query, (user,title,content))
+        db.commit()
+        db.close()
